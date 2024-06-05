e7e91df2af94abe74adc12415bd134e71a9e1b82
trung1007 <trungthanhcao.2003@gmail.com>
2024-06-06 03:13:36 +0700

    feat: remove Playlist

--- components/PlaylistSong.jsx
+++ components/PlaylistSong.jsx
@@ -4,44 +4,72 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { AuthProvider } from "../context/AuthContext";
 import themeContext from "../theme/themeContext";
 import { useNavigation } from "@react-navigation/native";
+import { FontAwesome } from "@expo/vector-icons";
+import {deleteDoc,doc, } from 'firebase/firestore'
+import { FIREBASE_DB } from "../config/firebase";
 
 const PlaylistSong = ({ PlaylistName, id, PlaylistList }) => {
   const theme = useContext(themeContext);
   const user = AuthProvider.user;
-const navigation = useNavigation()
+  const navigation = useNavigation();
+  const UserID  = user.id
 
-// useEffect(()=>{
-//   // console.log(PlaylistList);
-//   console.log(id);
-// },[])
+  // useEffect(()=>{
+  //   // console.log(PlaylistList);
+  //   console.log(id);
+  // },[])
+
+  const removePlaylist = async()=>{
+    const currentPlaylistDoc = doc(FIREBASE_DB, 'User',UserID, 'UserAlbum', id )
+    await deleteDoc(currentPlaylistDoc)
+  }
 
   return (
-    <TouchableOpacity style={styles.wrapper} onPress={()=>{
-        navigation.navigate('SongPlaylist',{PlaylistName, PlaylistList, id})
-    }}>
-      <View
-        style={{
-          backgroundColor: "white",
-          borderRadius: 10,
-          width: 60,
-          height: 60,
-          borderWidth:1,
-          alignItems: "center",
-          justifyContent: "center",
+    <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+      <TouchableOpacity
+        style={styles.wrapper}
+        onPress={() => {
+          navigation.navigate("SongPlaylist", {
+            PlaylistName,
+            PlaylistList,
+            id,
+          });
         }}
-        
       >
-        <Text style={{textTransform:'capitalize'}}>{PlaylistName}</Text>
-      </View>
-      <View>
-        <Text style={[styles.PlaylistName, { color: theme.color }]}>
-          {PlaylistName}
-        </Text>
-        <Text style={{ fontSize: 14, marginBottom: 10, color: "gray" }}>
-          {user.name}
-        </Text>
-      </View>
-    </TouchableOpacity>
+        <View
+          style={{
+            backgroundColor: "white",
+            borderRadius: 10,
+            width: 60,
+            height: 60,
+            borderWidth: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ textTransform: "capitalize" }}>{PlaylistName}</Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={[styles.PlaylistName, { color: theme.color }]}>
+              {PlaylistName}
+            </Text>
+            <Text style={{ fontSize: 14, marginBottom: 10, color: "gray" }}>
+              {user.name}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={{paddingRight:20}} onPress={removePlaylist}>
+        <FontAwesome name="trash" size={24} color={theme.color} />
+      </TouchableOpacity>
+    </View>
   );
 };
 const styles = StyleSheet.create({
@@ -56,7 +84,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
     marginTop: 5,
-    textTransform:'capitalize'
+    textTransform: "capitalize",
   },
 });
 
